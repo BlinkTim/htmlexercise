cc5f647bfa12900ec296100ac3872a5860776c05
BlinkTim <zetteronid@gmail.com>
2023-10-26 17:03:33 +0200

    added contact template

--- main.py
+++ main.py
@@ -12,11 +12,19 @@ templates = Jinja2Templates(directory="./")
 
 
 @app.get("/", response_class=HTMLResponse)
-async def read_item(request: Request):
+async def read_root(request: Request):
     name = "Tim Niggemann"
     skill1 = "HTML"
     skill2 = "Python"
     return templates.TemplateResponse("index.html", {"request": request, "name": name,"skill1": skill1, "skill2": skill2  })
+
+@app.get("/contact", response_class=HTMLResponse)
+async def read_root(request: Request):
+    return templates.TemplateResponse("contact.html", {"request": request}) 
+
+@app.post("/contact", response_class=HTMLResponse)
+async def submit(request: Request):
+    return templates.TemplateResponse("contact.html", {"request": request})
 
 if __name__ == "__main__":
         import uvicorn
